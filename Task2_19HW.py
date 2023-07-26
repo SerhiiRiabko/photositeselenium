7c5fec44386afad21317609a42297b5a1440d192
SerhiiRiabko <33356942+SerhiiRiabko@users.noreply.github.com>
2023-07-26 19:46:45 +0300

    Update Task2_19HW.py

--- Task2_19HW.py
+++ Task2_19HW.py
@@ -3,6 +3,10 @@ import json
 import xmltodict
 from human import Human
 
+parser = argparse.ArgumentParser(description="Convert Human instance to JSON or XML.")
+parser.add_argument("format", choices=["json", "xml"], help="Choose the format for conversion.")
+args = parser.parse_args()
+
 human = Human("John", 30, "male", 1993)
 
 if args.format == "json":
