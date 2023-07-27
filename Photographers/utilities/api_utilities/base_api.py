ce7a69ad6d08327c53fe687c245e44af46c3ce1e
Serhii-Study <adyvan2008@gmail.com>
2023-07-27 18:16:19 +0300

    last changes REST-base

--- Photographers/utilities/api_utilities/base_api.py
+++ Photographers/utilities/api_utilities/base_api.py
@@ -1,7 +1,4 @@
-import json
-
 import requests
-
 
 
 class BaseAPI:
@@ -16,8 +13,11 @@ class BaseAPI:
         response = self.__request.get(f'{self.__base_url}{url}', headers=headers)
         return response
 
-    def put(self):
-        pass
+    def put(self, url, body, headers=None):
+        if headers is None:
+            headers = self.__headers
+        response = self.__request.put(f'{self.__base_url}{url}', headers=headers, json=body)
+        return response
 
     def post(self, url, body, headers=None):
         if headers is None:
@@ -25,8 +25,14 @@ class BaseAPI:
         response = self.__request.post(f'{self.__base_url}{url}', json=body, headers=headers)
         return response
 
-    def patch(self):
-        pass
+    def patch(self, url, body, headers=None):
+        if headers is None:
+            headers = self.__headers
+        response = self.__request.patch(f'{self.__base_url}{url}', json=body, headers=headers)
+        return response
 
-    def delete(self):
-        pass
+    def delete(self, url, booking_id, headers=None):
+        if headers is None:
+            headers = self.__headers
+        response = self.__request.get(f'{self.__base_url}{url}{booking_id}', headers=headers)
+        return response
